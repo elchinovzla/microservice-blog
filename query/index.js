5d0d8873e39b21f8264b0982c949f07b928e6443
elchinovzla <sonnyng.vzla@gmail.com>
2023-06-08 14:16:02 -0600

    add object to keep track of events, so that it keeps track of missing events

--- query/index.js
+++ query/index.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const posts = {};
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get("/posts", (req, res) => {
-  res.status(200).send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
+const handleEvent = (type, data) => {
   const { id, content, postId, status } = data;
   const post = posts[postId];
 
@@ -31,9 +23,32 @@ app.post("/events", (req, res) => {
       comment.content = content;
       break;
   }
+};
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/posts", (req, res) => {
+  res.status(200).send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
   res.status(201);
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("query service listing in port 4002");
+
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+    for (let event of res.data) {
+      console.log("Processing event: " + event.type);
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log("error in query service: " + error);
+  }
 });
